b6be5ebe19be155e300e97ae958698719abf5d18
rajathongal <reachme.rajathongal@gmail.com>
2024-02-25 22:08:02 +0530

    improvements

--- k6.test.js
+++ k6.test.js
@@ -7,7 +7,7 @@ import { sleep } from "k6";
 
 export const options = {
   vus: 1000,
-  duration: "1s",
+  duration: "10s",
 };
 export default function () {
   describe("Transfers", () => {
@@ -20,49 +20,58 @@ export default function () {
 
     const randomUserA = JSON.parse(randomUserACreateResponse.body);
     const randomUserB = JSON.parse(randomUserBCreateResponse.body);
+    const { accountId } = randomUserA;
+    if (accountId) {
+      const senderAccountId = accountId;
+      const noncePayload = JSON.stringify({ accountId: senderAccountId });
 
-    const senderAccountId = randomUserA.accountId;
-    const noncePayload = JSON.stringify({ accountId: senderAccountId });
+      const nonceResponse = http.post(
+        "http://localhost:5001/accounts/get-nonce",
+        noncePayload,
+        {
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+      const { nonce } = JSON.parse(nonceResponse.body);
+      if (nonce) {
+        const getSignedTransactionPayload = JSON.stringify({
+          senderPublicKey: randomUserA.publicKey,
+          recipientPublicKey: randomUserB.publicKey,
+          senderPrivateKey: randomUserA.privateKey,
+          amount: "10.37",
+          nonce,
+        });
 
-    const nonceResponse = http.post(
-      "http://localhost:5001/accounts/get-nonce",
-      noncePayload,
-      {
-        headers: { "Content-Type": "application/json" },
+        const signedTransactionResponse = http.post(
+          "http://localhost:5001/accounts/get-signed-transaction",
+          getSignedTransactionPayload,
+          {
+            headers: { "Content-Type": "application/json" },
+          }
+        );
+        const transactionPayload = signedTransactionResponse.body;
+        const createTransferResponse = http.post(
+          "http://localhost:5001/accounts/create-transfer",
+          transactionPayload,
+          {
+            headers: { "Content-Type": "application/json" },
+          }
+        );
+        const { transactionId } = JSON.parse(
+          createTransferResponse.body
+        )
+        if (transactionId) {
+          
+          sleep(5);
+          const transferCompletionResponse = http.get(
+            `http://localhost:5001/accounts/getTransfer/${transactionId}`
+          );
+
+          expect(transferCompletionResponse.status, "response status").to.equal(
+            200
+          );
+        }
       }
-    );
-    const nonce = JSON.parse(nonceResponse.body).nonce;
-
-    const getSignedTransactionPayload = JSON.stringify({
-      senderPublicKey: randomUserA.publicKey,
-      recipientPublicKey: randomUserB.publicKey,
-      senderPrivateKey: randomUserA.privateKey,
-      amount: "10.37",
-      nonce,
-    });
-
-    const signedTransactionResponse = http.post(
-      "http://localhost:5001/accounts/get-signed-transaction",
-      getSignedTransactionPayload,
-      {
-        headers: { "Content-Type": "application/json" },
-      }
-    );
-    const transactionPayload = signedTransactionResponse.body;
-    const createTransferResponse = http.post(
-      "http://localhost:5001/accounts/create-transfer",
-      transactionPayload,
-      {
-        headers: { "Content-Type": "application/json" },
-      }
-    );
-
-    const transactionId = JSON.parse(createTransferResponse.body).transactionId;
-    sleep(5);
-    const transferCompletionResponse = http.get(
-      `http://localhost:5001/accounts/getTransfer/${transactionId}`
-    );
-
-    expect(transferCompletionResponse.status, "response status").to.equal(200);
+    }
   });
 }
--- server/utils/FIFOStack.js
+++ server/utils/FIFOStack.js
@@ -9,50 +9,77 @@ class UniqueFIFOStack {
   }
 
   async push(element) {
-    const lockAcquired = await this.redisClient.setNX(this.lockKey, "1");
+    let tryCount = 1;
+    let lockAcquiredFlag = false;
+    const max_retries = 100;
 
-    if (!lockAcquired) {
-      logger.warn("Unable to acquire lock, will retry later...");
-      return;
-    }
-
-    try {
-      // Check if the element is unique
-      const isUnique =
-        (await this.redisClient.sAdd(this.setKey, element)) === 1;
-
-      if (isUnique) {
-        // Add the element to the list
-        await this.redisClient.lPush(this.listKey, element);
-      } else {
-        logger.warn(`Element "${element}" already exists and won't be added.`);
+    retryLoop: while (!lockAcquiredFlag) {
+      if (tryCount > max_retries) {
+        logger.warn("Retries Maxed out");
+        break retryLoop;
       }
-    } finally {
-      // Ensure lock release even if errors occur
-      await this.redisClient.del(this.lockKey);
+      tryCount++;
+
+      const lockAcquired = await this.redisClient.setNX(this.lockKey, "1");
+
+      if (!lockAcquired) {
+        logger.warn("Unable to acquire lock, will retry later...");
+      }
+
+      try {
+        lockAcquiredFlag = true;
+
+        // Check if the element is unique
+        const isUnique =
+          (await this.redisClient.sAdd(this.setKey, element)) === 1;
+
+        if (isUnique) {
+          // Add the element to the list
+          await this.redisClient.lPush(this.listKey, element);
+        } else {
+          logger.warn(
+            `Element "${element}" already exists and won't be added.`
+          );
+        }
+      } finally {
+        // Ensure lock release even if errors occur
+        await this.redisClient.del(this.lockKey);
+      }
     }
   }
 
   async pop() {
-    const lockAcquired = await this.redisClient.setNX(this.lockKey, "1");
-
-    if (!lockAcquired) {
-      logger.warn("Unable to acquire lock, will retry later...");
-      return;
-    }
-    try {
-      // Remove the last element from the list
-      const poppedElement = await this.redisClient.rPop(this.listKey);
-
-      if (poppedElement) {
-        // Remove the popped element from the set to allow reinsertion
-        await this.redisClient.sRem(this.setKey, poppedElement);
+    let tryCount = 1;
+    let lockAcquiredFlag = false;
+    const max_retries = 100;
+    retryLoop: while (!lockAcquiredFlag) {
+      if (tryCount > max_retries) {
+        logger.warn("Retries Maxed out");
+        break retryLoop;
       }
+      tryCount++;
 
-      return poppedElement;
-    } finally {
-      // Ensure lock release even if errors occur
-      await this.redisClient.del(this.lockKey);
+      const lockAcquired = await this.redisClient.setNX(this.lockKey, "1");
+
+      if (!lockAcquired) {
+        logger.warn("Unable to acquire lock, will retry later...");
+        return;
+      }
+      try {
+        lockAcquiredFlag = true;
+        // Remove the last element from the list
+        const poppedElement = await this.redisClient.rPop(this.listKey);
+
+        if (poppedElement) {
+          // Remove the popped element from the set to allow reinsertion
+          await this.redisClient.sRem(this.setKey, poppedElement);
+        }
+
+        return poppedElement;
+      } finally {
+        // Ensure lock release even if errors occur
+        await this.redisClient.del(this.lockKey);
+      }
     }
   }
 
